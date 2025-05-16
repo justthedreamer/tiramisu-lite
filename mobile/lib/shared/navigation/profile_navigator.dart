7333306fab1eaa2c842f279aa3c3438e9f83b0ed
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-05-16 14:49:09 +0200

    profile navigatior implemented.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ mobile/lib/shared/navigation/profile_navigator.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/features/profile/screens/profile_page.dart';
+import 'package:mobile/model/profile.dart';
+import 'package:mobile/state/profile_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../../features/profile/screens/create_profile.dart';
+
+class ProfileNavigator {
+  static Future<bool> openCreateProfile({required BuildContext context}) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => const CreateProfilePage()),
+    );
+    return result == true;
+  }
+
+  static Future<bool> openProfile({
+    required BuildContext context,
+    required Profile destination,
+  }) async {
+    final profileState = context.read<ProfileState>();
+    profileState.setActiveProfile(destination);
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => ProfilePage()),
+    );
+    return result == true;
+  }
+}
