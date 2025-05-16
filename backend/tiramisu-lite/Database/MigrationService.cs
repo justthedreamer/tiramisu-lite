99b9e185414c997b16ddc0f937c7e271a2195104
Piotr <piotr.zapala.dev@gmail.com>
2025-05-16 18:48:39 +0200

    add migration service
    Signed-off-by: Piotr <piotr.zapala.dev@gmail.com>

---
+++ backend/tiramisu-lite/Database/MigrationService.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace tiramisu_lite.Database;
+
+public class MigrationService(IServiceProvider serviceProvider) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await dbContext.Database.MigrateAsync(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
