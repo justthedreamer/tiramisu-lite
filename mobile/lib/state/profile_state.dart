330a9b1532f62bb949450e918ec04943a8da22be
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-05-24 13:17:51 +0200

    states implemented
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- mobile/lib/state/profile_state.dart
+++ mobile/lib/state/profile_state.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/cupertino.dart';
-import 'package:mobile/model/profile.dart';
 
+import '../model/profile.dart';
 import '../shared/clients/profile_client.dart';
 
 class ProfileState extends ChangeNotifier {
   List<Profile> _profiles = [];
   Profile? _activeProfile;
+  String? error;
+  bool loading = false;
 
   List<Profile> get profiles => _profiles;
-
   Profile? get activeProfile => _activeProfile;
 
   void setProfiles(List<Profile> profiles) {
@@ -16,13 +17,24 @@ class ProfileState extends ChangeNotifier {
     notifyListeners();
   }
 
-  void setActiveProfile(Profile profile) {
+  void setActiveProfile(Profile? profile) {
     _activeProfile = profile;
     notifyListeners();
   }
 
   Future<void> fetchProfiles() async {
-    List<Profile> profiles = await ProfileClient.fetch();
-    setProfiles(profiles);
+    loading = true;
+    error = null;
+    notifyListeners();
+
+    try {
+      final fetched = await ProfileClient.fetch();
+      _profiles = fetched;
+    } catch (e) {
+      error = e.toString();
+    }
+
+    loading = false;
+    notifyListeners();
   }
 }
